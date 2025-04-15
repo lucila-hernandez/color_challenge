043b310c96e8395b59c92fee5f045a562ce58858
lucila-hernandez <lucila.hernandezdelcarmen@students.dominican.edu>
2025-04-15 15:27:07 -0700

    added correct/incorrect message

---
+++ challenge/src/Correctmessage.jsx
@@ -0,0 +1,13 @@
+function CorrectMessage() {
+    return (
+      <div style={{
+        width: '330px',
+        height: '30px',
+        backgroundColor: 'grey',
+        border: '2px solid black'
+      }}>
+      </div>
+    );
+  }
+
+export default CorrectMessage
--- challenge/src/main.jsx
+++ challenge/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RedSquare, BlueSquare, OrangeSquare } from './Square.jsx'
 import HexColor from './Hexcolor.jsx'
+import CorrectMessage from './Correctmessage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -10,5 +11,6 @@ createRoot(document.getElementById('root')).render(
     <RedSquare />
     <OrangeSquare />
     <BlueSquare />
+    <CorrectMessage/>
   </StrictMode>,
 )
